da4099e7094da72136d0f454c0625432d479298c
SebastianSebz <37335472+SebastianSebz@users.noreply.github.com>
2018-04-17 07:54:15 -0500

    Add files via upload

---
+++ evol.cpp
@@ -0,0 +1,56 @@
+//como evoluciona en el tiempo, cond. inicial = una funcion evaluada) se desplaza a vel c.
+// 0 a 2
+# include <iostream>
+
+using std::cout;
+using std::endl;
+
+void func(double *u, double xmin, double deltaX, int n);
+void evol();
+
+
+void func(double *u, double xmin, double deltaX, int n)
+{
+
+	for (int i=0; i<n; i++)
+	{	
+		double arr[i];
+
+		x = xmin + i*deltax
+
+		if (x>0.75 && x<=1.25)
+		{
+ 			arr[i] = 1.0
+		}
+		else
+		{ 
+			arr[i] = 0.0
+		}
+	}
+}
+
+void evol(){
+	
+	int i;
+
+	for (int i=0; i<2; i= i+0.1){
+		cons = c*deltat/deltax*(i*deltax + deltax)
+    		
+	}
+
+int main(){
+
+double *u, delta = 0.1, deltaX = 0.1, deltat = 0.01;
+double c = 1.0, T = 0.5;
+double xmax = 2.0, xmin = 0.0;
+
+int n_x;
+
+evol();
+}
+
+
+
+
+
+
